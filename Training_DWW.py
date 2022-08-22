f77b5e12062beaaebb17d01c0ad1ce58e1249abd
rajamani123 <111555982+rajamani123@users.noreply.github.com>
2022-08-22 23:58:31 +0530

    Update Training_DWW.py
    Added breakfast menu

--- Training_DWW.py
+++ Training_DWW.py
@@ -1,1 +1,9 @@
+import streamlit
 
+
+
+streamlit.title('my parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach and Rocket smoothie')
+streamlit.text('Hard boiled free range egg')  
