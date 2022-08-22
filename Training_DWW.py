48e115741b3290c1a070643bfef81ca7d4445783
rajamani123 <111555982+rajamani123@users.noreply.github.com>
2022-08-23 00:02:32 +0530

    Update Training_DWW.py
    Added Pandas

--- Training_DWW.py
+++ Training_DWW.py
@@ -6,4 +6,9 @@ streamlit.title('my parents new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega3 & Blueberry Oatmeal')
 streamlit.text('Kale,Spinach and Rocket smoothie')
-streamlit.text('Hard boiled free range egg')  
+streamlit.text('Hard boiled free range egg') 
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
