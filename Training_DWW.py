9517ce89a1324340d99cefb5e2ed34be85ef6e1c
rajamani123 <111555982+rajamani123@users.noreply.github.com>
2022-08-23 00:14:28 +0530

    Update Training_DWW.py
    Pandas

--- Training_DWW.py
+++ Training_DWW.py
@@ -1,5 +1,5 @@
 import pandas
-my_fruit_list = Pandas.read_csv("https://uni-lab-files.s3.us.west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = Pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
