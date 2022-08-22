c909265d42f11fbedf56c214667e3a642d58ec65
rajamani123 <111555982+rajamani123@users.noreply.github.com>
2022-08-23 00:15:55 +0530

    Update Training_DWW.py
    break fast

--- Training_DWW.py
+++ Training_DWW.py
@@ -1,8 +1,8 @@
-import pandas
-my_fruit_list = Pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+import streamlit
 
-streamlit.dataframe(my_fruit_list)
-
-
+streamlit.title('my parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach and Rocket smoothie')
+streamlit.text('Hard boiled free range egg') 
