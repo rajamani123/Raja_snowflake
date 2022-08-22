81279993ee427874fabe93c75d8a5814275c464d
rajamani123 <111555982+rajamani123@users.noreply.github.com>
2022-08-23 00:11:58 +0530

    Update Training_DWW.py
    PAndas

--- Training_DWW.py
+++ Training_DWW.py
@@ -1,17 +1,3 @@
-import streamlit
-
-
-
-streamlit.title('my parents new healthy dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach and Rocket smoothie')
-streamlit.text('Hard boiled free range egg') 
-
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
 import pandas
 my_fruit_list = Pandas.read_csv("https://uni-lab-files.s3.us.west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -19,5 +5,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
 
